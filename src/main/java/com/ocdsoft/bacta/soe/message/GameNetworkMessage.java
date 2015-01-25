70183e3529a46fdd3092d0764bcd0f0469d3a135
Kyle Burkhardt <kburkhardt@utah.gov>
2015-01-24 17:22:03 -0700

    [Update] GameNetworkMessage update

--- src/main/java/com/ocdsoft/bacta/soe/message/GameNetworkMessage.java
+++ src/main/java/com/ocdsoft/bacta/soe/message/GameNetworkMessage.java
@@ -5,7 +5,21 @@ import com.ocdsoft.bacta.engine.buffer.ByteBufferWritable;
 /**
  * Created by Kyle on 3/26/14.
  */
-public interface GameNetworkMessage extends ByteBufferWritable {
-    short getPriority();
-    int getMessageType();
+public abstract class GameNetworkMessage implements ByteBufferWritable {
+
+    private final short priority;
+    private final int messageType;
+
+    protected GameNetworkMessage(final short priority, final int messageType) {
+        this.priority = priority;
+        this.messageType = messageType;
+    }
+
+    public short getPriority() {
+        return priority;
+    }
+
+    public int getMessageType() {
+        return messageType;
+    }
 }
