c498914ae7b434a41ad1e5d35b8a4648a4c05ca0
Kyle Burkhardt <kburkhardt@utah.gov>
2015-01-31 23:19:51 -0700

    [Fix] Compile issue

--- src/main/java/com/ocdsoft/bacta/soe/chat/message/ChatAddFriend.java
+++ src/main/java/com/ocdsoft/bacta/soe/chat/message/ChatAddFriend.java
@@ -2,6 +2,7 @@ package com.ocdsoft.bacta.soe.chat.message;
 
 import com.ocdsoft.bacta.soe.chat.ChatAvatarId;
 import com.ocdsoft.bacta.soe.message.GameNetworkMessage;
+import com.ocdsoft.bacta.soe.util.SOECRC32;
 
 import java.nio.ByteBuffer;
 
@@ -9,15 +10,20 @@ import java.nio.ByteBuffer;
  * Created by crush on 1/12/2015.
  */
 public class ChatAddFriend extends GameNetworkMessage {
+
+    private static final short priority = 0x2; //TODO: Get the right value
+    private static final int messageType = SOECRC32.hashCode(ChatAddFriend.class.getSimpleName());
+
+
     private ChatAvatarId characterName;
     private int sequence;
 
-    public ChatAddFriend(int priority, int messageType) {
+    public ChatAddFriend() {
         super(priority, messageType);
     }
 
     @Override
-    public ByteBuffer toBuffer() {
-        return null;
+    public void writeToBuffer(ByteBuffer buffer) {
+
     }
 }
