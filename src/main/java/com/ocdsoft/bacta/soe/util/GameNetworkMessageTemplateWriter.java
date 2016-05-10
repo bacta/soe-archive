a0abc7d39609338797f264ac6d1d853e1f2ed24a
Kyle Burkhardt <mustangcoupe69@gmail.com>
2016-05-09 19:08:12 -0600

    [Fix] Datatable writing - pending iff completion

--- src/main/java/com/ocdsoft/bacta/soe/util/GameNetworkMessageTemplateWriter.java
+++ src/main/java/com/ocdsoft/bacta/soe/util/GameNetworkMessageTemplateWriter.java
@@ -17,6 +17,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.nio.ByteBuffer;
+import java.nio.file.Paths;
 
 /**
  * Created by kburkhardt on 1/31/15.
